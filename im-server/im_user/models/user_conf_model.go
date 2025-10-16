0ce74e3f53e374f22b3f572787efaecbf626a73b
杨忠宇 <kane.yang@jaco.live>
2025-10-16 21:58:35 +0800

    fix
    Signed-off-by: 杨忠宇 <kane.yang@jaco.live>

--- im-server/im_user/models/user_conf_model.go
+++ im-server/im_user/models/user_conf_model.go
@@ -16,10 +16,10 @@ type UserConfModel struct {
 }
 
 type VerificationQuestion struct {
-	Question1 *string `gorm:"column:question" json:"question"` //验证问题
-	Answer1   *string `gorm:"column:answer" json:"answer"`     //验证问题的答案
-	Question2 *string `gorm:"column:question" json:"question"` //验证问题
-	Answer2   *string `gorm:"column:answer" json:"answer"`     //验证问题的答案
-	Question3 *string `gorm:"column:question" json:"question"` //验证问题
-	Answer3   *string `gorm:"column:answer" json:"answer"`     //验证问题的答案
+	Question1 *string `gorm:"column:question" json:"question1"` //验证问题
+	Answer1   *string `gorm:"column:answer" json:"answer1"`     //验证问题的答案
+	Question2 *string `gorm:"column:question" json:"question2"` //验证问题
+	Answer2   *string `gorm:"column:answer" json:"answer2"`     //验证问题的答案
+	Question3 *string `gorm:"column:question" json:"question3"` //验证问题
+	Answer3   *string `gorm:"column:answer" json:"answer3"`     //验证问题的答案
 }
