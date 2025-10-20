fc9c2767bef69f0d97e2eeed4e566332d27e5b2c
杨忠宇 <kane.yang@jaco.live>
2025-10-20 10:55:15 +0800

    fix
    Signed-off-by: 杨忠宇 <kane.yang@jaco.live>

--- im-server/im_group/group_models/group_models.go
+++ im-server/im_group/group_models/group_models.go
@@ -7,10 +7,10 @@ import (
 
 type GroupModel struct {
 	models.Model
-	Title                string                      `gorm:"column:title" json:"title"`       //群名称
-	Abstract             string                      `gorm:"column:abstract" json:"abstract"` //群描述
-	Avatar               string                      `gorm:"column:avatar" json:"avatar"`     //群头像
-	Creator              uint                        `gorm:"column:creator" json:"creator"`   //群创建者
+	Title                string                      `gorm:"column:title;size:255" json:"title"`       //群名称
+	Abstract             string                      `gorm:"column:abstract;size:255" json:"abstract"` //群描述
+	Avatar               string                      `gorm:"column:avatar;size:255" json:"avatar"`     //群头像
+	Creator              uint                        `gorm:"column:creator" json:"creator"`            //群创建者
 	IsSearchable         bool                        `gorm:"column:is_searchable" json:"is_searchable"`
 	Verification         uint                        `gorm:"column:verification" json:"verification"`                   // 好友验证方式 0:不允许任何人添加 1:允许任何人添加 2：需要验证消息 3:需要回答问题 4:需要正确回答问题
 	VerificationQuestion *ctype.VerificationQuestion `gorm:"column:verification_question" json:"verification_question"` // 验证问题
--- im-server/im_user/user_models/user_model.go
+++ im-server/im_user/user_models/user_model.go
@@ -4,10 +4,10 @@ import "im-server/common/models"
 
 type UserModel struct {
 	models.Model
-	Pwd      string `gorm:"column:password" json:"password"`
-	Nickname string `gorm:"column:nickname" json:"nickname"`
-	Abstract string `gorm:"column:abstract" json:"abstract"` //用户简介
-	Avatar   string `gorm:"column:avatar" json:"avatar"`     //用户头像
-	IP       string `gorm:"column:ip" json:"ip"`             //用户登录IP
-	Addr     string `gorm:"column:addr" json:"addr"`         //用户登录地址
+	Pwd      string `gorm:"column:password;size:255" json:"password"`
+	Nickname string `gorm:"column:nickname;size:255" json:"nickname"`
+	Abstract string `gorm:"column:abstract;size:255" json:"abstract"` //用户简介
+	Avatar   string `gorm:"column:avatar;size:255" json:"avatar"`     //用户头像
+	IP       string `gorm:"column:ip;size:255" json:"ip"`             //用户登录IP
+	Addr     string `gorm:"column:addr;size:255" json:"addr"`         //用户登录地址
 }
