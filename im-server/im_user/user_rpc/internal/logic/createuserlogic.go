fb22e0d93b428ed5465f27a461f447a3087f5891
杨忠宇 <kane.yang@jaco.live>
2025-10-29 19:21:40 +0800

    fix
    Signed-off-by: 杨忠宇 <kane.yang@jaco.live>

---
+++ im-server/im_user/user_rpc/internal/logic/createuserlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"im-server/im_user/user_rpc/internal/svc"
+	"im-server/im_user/user_rpc/types/user_rpc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type CreateUserLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewCreateUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CreateUserLogic {
+	return &CreateUserLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *CreateUserLogic) CreateUser(in *user_rpc.UserCreateRequest) (*user_rpc.UserCreateResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &user_rpc.UserCreateResponse{}, nil
+}
---
+++ im-server/im_user/user_rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "im-server/im_user/user_rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
