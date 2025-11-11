afe3e3c4a6ea0dca69042bc235c7993615dfdfc7
杨忠宇 <kane.yang@jaco.live>
2025-11-11 21:04:12 +0800

    fix
    Signed-off-by: 杨忠宇 <kane.yang@jaco.live>

--- im-webapp/src/views/login/index.vue
+++ im-webapp/src/views/login/index.vue
@@ -1,29 +1,31 @@
 <template>
-  <div class="fim_login">
-    <div class="banner">
+  <div class="container">
+    <div class="fim_login">
+      <div class="banner">
 
-    </div>
-    <div class="login_form">
-      <el-form :model="form">
-        <el-form-item>
-          <el-input type="text" v-model="form.user_name" placeholder="用户名"/>
-        </el-form-item>
-        <el-form-item>
-          <el-input type="password" v-model="form.password" placeholder="密码"/>
-        </el-form-item>
-        <el-form-item>
-          <el-checkbox>记住密码</el-checkbox>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="login" style="width: 100%;">登录</el-button>
-        </el-form-item>
-       </el-form>
-    </div>
-
-    <div class="bottom">
-      <router-link to="/register">注册账号</router-link>
+      </div>
+      <div class="login_form">
+        <el-form :model="form">
+          <el-form-item>
+            <el-input type="text" v-model="form.user_name" placeholder="用户名"/>
+          </el-form-item>
+          <el-form-item>
+            <el-input type="password" v-model="form.password" placeholder="密码"/>
+          </el-form-item>
+          <el-form-item>
+            <el-checkbox>记住密码</el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="login" style="width: 100%;">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="bottom">
+        <router-link to="/register">注册账号</router-link>
+      </div>
     </div>
   </div>
+
 </template>
 
 
@@ -67,6 +69,10 @@ async function login() {
 
 <style scoped lang="scss">
 $color: #f0f0f0;
+.container {
+  display: flex;
+  height: 100vh;
+}
 
 .fim_login {
   background: $color;
