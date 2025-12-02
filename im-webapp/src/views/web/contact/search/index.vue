e5ac8bac5820110897f70104071e93d3728ce56b
杨忠宇 <kane.yang@jaco.live>
2025-12-02 21:26:40 +0800

    fix
    Signed-off-by: 杨忠宇 <kane.yang@jaco.live>

--- im-webapp/src/views/web/contact/search/index.vue
+++ im-webapp/src/views/web/contact/search/index.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import {useRouter} from "vue-router";
 import {searchUser, type SearchUserRequest} from "@/api/user_api";
+import {ref} from "vue";
 
 const router = useRouter()
+
+const searchTxt = ref('')
 function changeTab(index: number) {
     // TODO: change tab
     if (index == 0) {
@@ -22,12 +25,20 @@ function changeTab(index: number) {
 </script>
 
 <template>
+  <div class="contact-search">
+    <el-input v-model="searchTxt" placeholder="请输入用户号或者用户昵称"></el-input>
+    <el-button @click="changeTab(0)">搜索</el-button>
+  </div>
   <el-button @click="changeTab(0)">找人</el-button>
   <el-button @click="changeTab(1)">找群</el-button>
+
  <router-view></router-view>
 
 </template>
 
 <style scoped>
-
+.contact-search {
+  display: flex;
+  align-items: center;
+}
 </style>
--- im-webapp/src/views/web/notice/index.vue
+++ im-webapp/src/views/web/notice/index.vue
@@ -2,6 +2,7 @@
 
 import {onMounted} from "vue";
 import {getFriendApplyList} from "@/api/user_api";
+import { ArrowDown } from '@element-plus/icons-vue';
 
 onMounted(() => {
   //获取我的好友申请验证列表
@@ -9,26 +10,33 @@ onMounted(() => {
     console.log(res)
   })
 })
+
+function handleCommand(command: string) {
+  console.log(command);
+}
+
 </script>
-
 <template>
-<div>
-  <el-button-group>
-    <el-button type="primary">通知</el-button>
-    <el-button type="primary">私信</el-button>
-    <el-dropdown>
-      <el-button type="primary">通知</el-button>
-      <el-button type="primary">私信</el-button>
-      <el-dropdown-menu>
-        <el-dropdown-item>通知</el-dropdown-item>
-        <el-dropdown-item>私信</el-dropdown-item>
-      </el-dropdown-menu>
-    </el-dropdown>
-  </el-button-group>
-</div>
+  <el-dropdown @click="handleCommand">
+    <span class="el-dropdown-link">
+      下拉菜单<el-icon class="el-icon--right"><arrow-down /></el-icon>
+    </span>
 
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>选项一</el-dropdown-item>
+        <el-dropdown-item divided>分割线</el-dropdown-item>
+        <el-dropdown-item disabled>禁用选项</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
 </template>
 
 <style scoped>
-
+.el-dropdown-link {
+  cursor: pointer;
+  color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
+}
 </style>
