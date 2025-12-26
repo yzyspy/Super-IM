f17c5bafe398f2670091a991b3e8565de8f9bdd8
杨忠宇 <kane.yang@jaco.live>
2025-12-26 14:43:42 +0800

    fix
    Signed-off-by: 杨忠宇 <kane.yang@jaco.live>

--- im-webapp/src/views/web/contact/welcome.vue
+++ im-webapp/src/views/web/contact/welcome.vue
@@ -3,13 +3,28 @@
 
 <template>
   <div class="container">
-    <div>主要功能</div>
-    <div>用户注册、用户登录、用户修改个人信息、添加好友、单聊、创建群组、群聊、发送图片和语音、视频通话</div>
+    <div class="title">欢迎使用FIM即时通讯</div>
+    <div>
+      <div>
+        这是一款基于vue3+go-zero的前后端微服务项目
+        前端:vue3typescript acroDesgin WebRTC
+        后端:go-zero gorm elasticsearch websocket
+      </div>
+      基本功能:
+      <ul>
+        <li>友单聊功能,支持文字、表情、图片、文件</li>
+        <li>好友搜索,好友申请</li>
+        <li>用户群聊功能,群文件、群禁言、管理员、群搜索 </li>
+        <li>用户视频通话功能 </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .container {
-
+   .title {
+     font-size: 40px;
+   }
 }
 </style>
