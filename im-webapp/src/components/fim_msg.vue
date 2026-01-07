4f07ac466051c47c849015f9e79747864ae01f86
杨忠宇 <kane.yang@jaco.live>
2026-01-08 00:12:46 +0800

    fix
    Signed-off-by: 杨忠宇 <kane.yang@jaco.live>

--- im-webapp/src/components/fim_msg.vue
+++ im-webapp/src/components/fim_msg.vue
@@ -14,9 +14,9 @@ const props = defineProps<Props>()
 
 <template>
 <div class="fim_msg">
-   <el-avatar :src="props.data.user.avatar" size="small"></el-avatar>
   <div class="info">
-    <el-text>{{ props.data.user.nickName }}</el-text>
+<!--    <el-text>{{ props.data.user.nickName }}</el-text>-->
+    <el-avatar :src="props.data.user.avatar" size="small"></el-avatar>
     <div class="msg">
       <div class="text_msg">
         {{props.data.msg.textMsg.content}}
@@ -27,5 +27,12 @@ const props = defineProps<Props>()
 </template>
 
 <style scoped>
-
+.info {
+  display: flex;
+  margin-top: 20px;
+}
+.text_msg {
+  background-color: #D5E2FC;
+  margin-left: 10px;
+}
 </style>
